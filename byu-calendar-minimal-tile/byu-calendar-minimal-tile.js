104929b61075594d9c99e4565bcfe8164bc9d709
William Wallace <willtwall@googlemail.com>
2018-06-27 16:16:54 -0600

    Added files to start working on the minimal tile component.

---
+++ byu-calendar-minimal-tile/byu-calendar-minimal-tile.js
@@ -0,0 +1,7 @@
+'use strict';
+const util = require('byu-web-component-utils');
+const minimalTemplate = require('./byu-calendar-minimal-tile.html');
+
+class ByuCalendarMinimalTile extends HTMLelement {
+
+}
